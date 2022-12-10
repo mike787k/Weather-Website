35b194b182e798b253e886b0bd5d68072207d0ea
Shermol <shermol.peters01@gmail.com>
2022-12-10 14:59:24 -0500

    comments made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-function App() {
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function App(){
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0649db80359931b5e91fd867a95060ba`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+  
   return (
-    <div className="App">
+    <div className='app'>
+      <div className='input'>
+        <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
+      </div>
+      <div className='container'>
+        <div classname='location'>
+          <p>{data.name}</p>
+        </div>
+        <div className='temprature'>
+          {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+        </div>
+        <div className='description'>
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+        </div>
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
